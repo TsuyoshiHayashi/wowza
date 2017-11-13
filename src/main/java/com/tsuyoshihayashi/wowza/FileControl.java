78a38bda1babb6cc36056ce4d790f1f5bc707438
Alexey Donov <alexeydonov@me.com>
2017-11-13 09:20:16 +0300

    File control API

--- src/main/java/com/tsuyoshihayashi/wowza/FileControl.java
+++ src/main/java/com/tsuyoshihayashi/wowza/FileControl.java
@@ -3,9 +3,16 @@ package com.tsuyoshihayashi.wowza;
 import com.wowza.wms.http.HTTPProvider2Base;
 import com.wowza.wms.http.IHTTPRequest;
 import com.wowza.wms.http.IHTTPResponse;
+import com.wowza.wms.logging.WMSLogger;
+import com.wowza.wms.logging.WMSLoggerFactory;
 import com.wowza.wms.vhost.IVHost;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * @author Alexey Donov
@@ -14,6 +21,9 @@ public final class FileControl extends HTTPProvider2Base {
     private static final String ACTION_PARAMETER_NAME = "a";
     private static final String ACTION_LIST = "list";
     private static final String ACTION_DELETE = "delete";
+    private static final String FILE_PARAMETER_NAME = "f";
+
+    private final WMSLogger logger = WMSLoggerFactory.getLogger(FileControl.class);
 
     private void writeResponse(IHTTPResponse response, int code, String body) {
         response.setResponseCode(code);
@@ -25,37 +35,69 @@ public final class FileControl extends HTTPProvider2Base {
         }
     }
 
-    private void writeResponse(IHTTPResponse response, int code) {
-        writeResponse(response, code, null);
+    private void writeBadRequestResponse(IHTTPResponse response) {
+        writeResponse(response, 400, null);
     }
 
+    @SuppressWarnings("unchecked")
+    private JSONObject fileData(File file) {
+        final JSONObject result = new JSONObject();
+        result.put("name", file.getName());
+        result.put("size", file.length());
+        result.put("date", file.lastModified() / 1000);
+
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
     @Override
     public void onHTTPRequest(IVHost host, IHTTPRequest request, IHTTPResponse response) {
-        if (!"GET".equals(request.getMethod())) {
-            writeResponse(response, 400);
-            return;
+        try {
+            if (!"GET".equals(request.getMethod())) {
+                writeBadRequestResponse(response);
+                return;
+            }
+
+            final String action = request.getParameter(ACTION_PARAMETER_NAME);
+            if (action == null || action.isEmpty()) {
+                writeBadRequestResponse(response);
+                return;
+            }
+
+            final String contentRoot = "/usr/local/WowzaStreamingEngine/content";
+
+            switch (action) {
+                case ACTION_LIST:
+                    final File root = new File(contentRoot);
+                    final JSONArray files = new JSONArray();
+                    Optional.ofNullable(root.listFiles(File::isFile))
+                        .ifPresent(fileArray -> Stream.of(fileArray)
+                            .map(this::fileData)
+                            .forEach(files::add));
+                    writeResponse(response, 200, files.toJSONString());
+                    return;
+
+                case ACTION_DELETE:
+                    final String fileName = request.getParameter(FILE_PARAMETER_NAME);
+                    if (fileName == null || fileName.isEmpty()) {
+                        writeBadRequestResponse(response);
+                        return;
+                    }
+                    final File file = new File(contentRoot.concat("/").concat(fileName));
+                    if (file.exists()) {
+                        logger.info(String.format("Deleting file %s by request", file.getName()));
+                        //noinspection ResultOfMethodCallIgnored
+                        file.delete();
+                    }
+                    writeResponse(response, 200, "{\"ok\": true}");
+                    return;
+
+                default:
+                    writeBadRequestResponse(response);
+                    break;
+            }
+        } catch (Exception e) {
+            writeResponse(response, 500, e.getMessage());
         }
-
-        final String action = request.getParameter(ACTION_PARAMETER_NAME);
-        if (action == null || action.isEmpty()) {
-            writeResponse(response, 400);
-            return;
-        }
-
-        switch (action) {
-            case ACTION_LIST:
-                // TODO: Collect files and make json array
-                break;
-
-            case ACTION_DELETE:
-                // TODO: Delete single file
-                break;
-
-            default:
-                response.setResponseCode(400);
-                break;
-        }
-
-        writeResponse(response, 200, "{\"ok\": true}");
     }
 }
