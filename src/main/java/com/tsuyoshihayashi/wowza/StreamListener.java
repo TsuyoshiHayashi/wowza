1ec99432d84507ae2792f932ac4b4b0b8c1a1df7
Alexey Donov <alexeydonov@me.com>
2017-11-13 09:18:35 +0300

    Push publish

--- src/main/java/com/tsuyoshihayashi/wowza/StreamListener.java
+++ src/main/java/com/tsuyoshihayashi/wowza/StreamListener.java
@@ -1,12 +1,15 @@
 package com.tsuyoshihayashi.wowza;
 
 import com.tsuyoshihayashi.model.RecordSettings;
+import com.wowza.wms.application.ApplicationInstance;
 import com.wowza.wms.application.IApplicationInstance;
 import com.wowza.wms.application.WMSProperties;
 import com.wowza.wms.livestreamrecord.manager.IStreamRecorderConstants;
 import com.wowza.wms.livestreamrecord.manager.StreamRecorderParameters;
 import com.wowza.wms.logging.WMSLogger;
 import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.pushpublish.protocol.rtmp.PushPublishRTMP;
+import com.wowza.wms.server.LicensingException;
 import com.wowza.wms.stream.IMediaStream;
 import com.wowza.wms.stream.MediaStreamActionNotifyBase;
 import org.json.simple.JSONObject;
@@ -15,6 +18,8 @@ import org.json.simple.parser.ParseException;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+
+import java.util.Optional;
 
 import static com.tsuyoshihayashi.model.RecordSettings.fromJSON;
 import static com.tsuyoshihayashi.wowza.StreamConstants.RECORD_SETTINGS_KEY;
@@ -26,6 +31,9 @@ import static java.util.concurrent.CompletableFuture.completedFuture;
 final class StreamListener extends MediaStreamActionNotifyBase {
     private static final String API_ENDPOINT_KEY = "apiEndpoint";
     private static final String UPLOAD_REFERER_KEY = "uploadReferer";
+    private static final String PUSH_HOST_KEY = "pushHost";
+    private static final String PUSH_APP_KEY = "pushApp";
+    private static final String PUBLISHER_PROPERTY_NAME = "publisher";
     private static final String API_STREAM_NAME_PARAMETER_NAME = "n";
 
     private final WMSLogger logger = WMSLoggerFactory.getLogger(StreamListener.class);
@@ -35,6 +43,8 @@ final class StreamListener extends MediaStreamActionNotifyBase {
     private final IApplicationInstance instance;
     private final String apiEndpoint;
     private final String uploadReferer;
+    private final String pushHost;
+    private final String pushApp;
 
     StreamListener(IApplicationInstance instance) {
         super();
@@ -45,6 +55,8 @@ final class StreamListener extends MediaStreamActionNotifyBase {
 
         this.apiEndpoint = properties.getPropertyStr(API_ENDPOINT_KEY);
         this.uploadReferer = properties.getPropertyStr(UPLOAD_REFERER_KEY, "");
+        this.pushHost = properties.getPropertyStr(PUSH_HOST_KEY);
+        this.pushApp = properties.getPropertyStr(PUSH_APP_KEY);
 
         logger.info(String.format("API Endpoint: %s", apiEndpoint));
     }
@@ -90,10 +102,40 @@ final class StreamListener extends MediaStreamActionNotifyBase {
                     instance.getVHost().getLiveStreamRecordManager().startRecording(instance, name, parameters);
                 }
             }).whenComplete((v, t) -> logger.error(t.getMessage()));
+
+        Optional.ofNullable(pushHost)
+            .ifPresent(host -> {
+                logger.info(String.format("Pushing %s stream to %s", stream.getName(), host));
+                try {
+                    final PushPublishRTMP publisher = new PushPublishRTMP();
+                    publisher.setAppInstance(stream.getClient().getAppInstance());
+                    publisher.setSrcStream(stream);
+                    publisher.setHost(host);
+                    publisher.setDstApplicationName(pushApp == null ? "live" : pushApp);
+                    publisher.setDstAppInstanceName(ApplicationInstance.DEFAULT_APPINSTANCE_NAME);
+                    publisher.setDstStreamName(stream.getName());
+                    publisher.setConnectionFlashVerion(PushPublishRTMP.CURRENTFLASHVERSION);
+                    publisher.setSendFCPublish(true);
+                    publisher.setSendReleaseStream(true);
+                    publisher.setSendOnMetadata(true);
+
+                    publisher.connect();
+                    stream.getProperties().setProperty(PUBLISHER_PROPERTY_NAME, publisher);
+                } catch (LicensingException e) {
+                    logger.error("No license for PushPublish");
+                }
+            });
     }
 
     @Override
     public void onUnPublish(IMediaStream stream, String name, boolean record, boolean append) {
         instance.getVHost().getLiveStreamRecordManager().stopRecording(instance, name);
+
+        Optional.ofNullable(stream.getProperties().getProperty(PUBLISHER_PROPERTY_NAME))
+            .map(PushPublishRTMP.class::cast)
+            .ifPresent(publisher -> {
+                publisher.disconnect();
+                stream.getProperties().remove(PUBLISHER_PROPERTY_NAME);
+            });
     }
 }
